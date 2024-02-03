cfa5b4df8bb7192d8cde79d4d409254ffeb7f280
muhallimir <mir23wpurposes@gmail.com>
2024-02-03 21:26:28 +0800

    feat: card selection on click and swipe

--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useRef, useState } from 'react';
 import { Box, Button } from '@mui/material';
 import { Swiper, SwiperSlide } from 'swiper/react';
 import 'swiper/css';
@@ -17,73 +17,73 @@ export default function Home() {
 	const { games } = useSelector(({ game }) => game);
 	const { theme: mode } = useSelector(({ app }) => app);
 	const darkMode = mode === 'dark';
-
+	const [selected, setSelected] = useState(0)
 	const router = useRouter()
+	const swiperRef = useRef(null);
 
-	const cardStyles = {
-		card: {
-			display: 'flex',
-			flexDirection: 'column',
-			border: `1px solid ${darkMode ? 'var(--common-white)' : 'var(--common-black)'}`,
-			borderRadius: '12px',
-			gap: '2px',
-			width: '100%',
-			height: '100%',
-			overflow: 'hidden',
-			position: 'relative',
-			cursor: 'pointer',
-			'&:hover, &:focus': {
-				border: `2px solid var(--primary-ast-main)`,
+
+
+	const cardStyles = (selected) => {
+		return {
+			card: {
+				display: 'flex',
+				flexDirection: 'column',
+				border: `1px solid ${darkMode ? 'var(--common-white)' : 'var(--common-black)'}`,
+				borderRadius: '12px',
+				gap: '2px',
+				width: '100%',
+				cursor: 'pointer',
+				...(selected && { border: '2px solid var(--primary-ast-main)' }),
+				[Theme.breakpoints.down('xs')]: {
+					maxHeight: '178px',
+				},
+				[Theme.breakpoints.down('sm')]: {
+					maxHeight: '178px',
+				},
+				[Theme.breakpoints.between('md', 'lg')]: {
+					height: '198px',
+				},
+				[Theme.breakpoints.up('lg')]: {
+					height: '199px',
+				},
+				[Theme.breakpoints.up('xl')]: {
+					height: '228px',
+				},
 			},
-			[Theme.breakpoints.down('xs')]: {
-				maxHeight: '178px',
+			cardImage: {
+				width: '100%',
+				objectFit: 'cover',
+				borderTopRightRadius: '10px',
+				borderTopLeftRadius: '10px',
+				[Theme.breakpoints.down('xs')]: {
+					minHeight: '151px',
+				},
+				[Theme.breakpoints.down('sm')]: {
+					height: '151px',
+				},
+				[Theme.breakpoints.between('md', 'lg')]: {
+					height: '198px',
+				},
+				[Theme.breakpoints.up('lg')]: {
+					height: '199px',
+				},
+				[Theme.breakpoints.up('xl')]: {
+					height: '180px',
+				},
 			},
-			[Theme.breakpoints.down('sm')]: {
-				maxHeight: '178px',
+			title: {
+				wrapper: {
+					display: 'flex', gap: '5px', alignItems: 'center', padding: '10px'
+				},
+				text: {
+					color: darkMode ? 'common.white' : 'common.black'
+				},
+				icon: {
+					width: 24,
+					height: 24
+				}
 			},
-			[Theme.breakpoints.between('md', 'lg')]: {
-				height: '198px',
-			},
-			[Theme.breakpoints.up('lg')]: {
-				height: '199px',
-			},
-			[Theme.breakpoints.up('xl')]: {
-				height: '228px',
-			},
-		},
-		cardImage: {
-			width: '100%',
-			objectFit: 'cover',
-			borderTopRightRadius: '10px',
-			borderTopLeftRadius: '10px',
-			[Theme.breakpoints.down('xs')]: {
-				minHeight: '151px',
-			},
-			[Theme.breakpoints.down('sm')]: {
-				height: '151px',
-			},
-			[Theme.breakpoints.between('md', 'lg')]: {
-				height: '198px',
-			},
-			[Theme.breakpoints.up('lg')]: {
-				height: '199px',
-			},
-			[Theme.breakpoints.up('xl')]: {
-				height: '180px',
-			},
-		},
-		title: {
-			wrapper: {
-				display: 'flex', gap: '5px', alignItems: 'center', padding: '10px'
-			},
-			text: {
-				color: darkMode ? 'common.white' : 'common.black'
-			},
-			icon: {
-				width: 24,
-				height: 24
-			}
-		},
+		}
 	}
 
 	const breakpoints = {
@@ -97,15 +97,15 @@ export default function Home() {
 		},
 		[Theme.breakpoints.values.md]: {
 			slidesPerView: 4.75,
-			spaceBetween: 8,
+			spaceBetween: 12,
 		},
 		[Theme.breakpoints.values.lg]: {
 			slidesPerView: 6,
-			spaceBetween: 16,
+			spaceBetween: 12,
 		},
 		[Theme.breakpoints.values.xl]: {
 			slidesPerView: 6,
-			spaceBetween: 16,
+			spaceBetween: 12,
 		},
 	};
 
@@ -113,18 +113,33 @@ export default function Home() {
 		router.push({ pathname: '/all-games' })
 	}
 
+	const handleSlideClick = (index) => {
+		if (index !== selected) {
+			swiperRef.current.swiper.slideTo(index, undefined, undefined, true);
+		}
+
+		setSelected(index);
+	};
+
 	return (
 		<>
 			<Header />
 			<Box sx={{
 				height: '100vh',
 				mt: '55px',
-				px: {
+				mr: {
+					xs: '0',
+					sm: '0px',
+					md: '0px',
+					lg: '50px',
+					xl: '100px',
+				},
+				ml: {
 					xs: '8px',
 					sm: '16px',
 					md: '16px',
-					lg: '24px',
-					xl: '24px',
+					lg: '50px',
+					xl: '100px',
 				},
 			}}>
 				<Button
@@ -148,6 +163,8 @@ export default function Home() {
 					position: 'relative',
 				}}>
 					<Swiper
+						ref={swiperRef}
+						initialSlide={selected}
 						breakpoints={breakpoints}
 						pagination={{
 							el: '.swiper-pagination',
@@ -156,16 +173,19 @@ export default function Home() {
 								return '<span class="' + className + '"></span>';
 							},
 						}}
+						onSlideChange={(swiper) => setSelected(swiper.realIndex)}
 						navigation={{
 							nextEl: '.swiper-button-next',
 							prevEl: '.swiper-button-prev',
 						}}
 					>
-						{games?.map((gameDetails, i) => (
-							<SwiperSlide key={i}>
-								<GameCard game={gameDetails} styles={cardStyles} />
-							</SwiperSlide>
-						))}
+						{games?.map((gameDetails, i) => {
+							return (
+								<SwiperSlide key={i} onMouseEnter={() => setSelected(i)} onClick={() => handleSlideClick(i)}>
+									<GameCard i={i} game={gameDetails} styles={cardStyles(selected === i)} />
+								</SwiperSlide>
+							)
+						})}
 					</Swiper>
 					<Box className="swiper-pagination" sx={{ position: 'absolute', bottom: '-10px', textAlign: 'center', width: '100%', display: 'flex', justifyContent: 'center' }} />
 				</Box>
